74736db45334ac4046b4021f64fbbc516796d493
Ian Vergel <cianvergelll@gmail.com>
2024-11-06 23:36:11 +0800

    Error message auth register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -10,17 +10,31 @@ export default function Register() {
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
 
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!name || !email || !password) {
+            setError("All fields are necessary.");
+            return;
+        }
+    };
+
+    console.log("Name: ", name);
     return (
         <div className="h-screen flex overflow-hidden">
             {/*Left- Container*/}
             <div className= "h-full flex flex-col w-[50%] bg-gradient-to-br from-[#018053] to-[#001a10] justify-center items-center rounded-tl-xl rounded-bl-xl transition-transform duration-500">
-            <form className="flex flex-col w-[50%]">
-                <input type="text" placeholder="Username" className="bg-transparent border-2 border-white text-white placeholder-white mb-4 px-4 py-3 rounded-full transition-all duration-500"></input>
-                <input type="email" placeholder="Email" className="bg-transparent border-2 border-white text-white placeholder-white mb-4 px-4 py-3 rounded-full transition-all duration-500"></input>
-                <input type="password" placeholder="Password" className="bg-transparent border-2 border-white text-white placeholder-white mb-4 px-4 py-3 rounded-full transition-all duration-500"></input>
-                <input type="password" placeholder="Confirm Password" className="bg-transparent border-2 border-white text-white placeholder-white mb-4 px-4 py-3 rounded-full transition-all duration-500"></input>
+
+            <form onSubmit={handleSubmit} className="flex flex-col w-[50%]">
+                <input onChange={e => setName(e.target.value)} type="text" placeholder="Username" className="bg-transparent border-2 border-white text-white placeholder-white mb-4 px-4 py-3 rounded-full transition-all duration-500"></input>
+                <input onChange={e => setEmail(e.target.value)} type="email" placeholder="Email" className="bg-transparent border-2 border-white text-white placeholder-white mb-4 px-4 py-3 rounded-full transition-all duration-500"></input>
+                <input onChange={e => setPassword(e.target.value)} type="password" placeholder="Password" className="bg-transparent border-2 border-white text-white placeholder-white mb-4 px-4 py-3 rounded-full transition-all duration-500"></input>
                 <Link className="bg-white py-3 rounded-full block text-center hover:bg-[#018053] hover:text-white transition-all duration-500" href="/register">Register</Link>
-                <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">Error Message</div>
+
+                { error && (
+                <div className="bg-red-500 text-white w-fit text-sm py-1 px-3 rounded-md mt-2">{error}</div>
+                )}
+
                 <a href="/login" className="text-white text-center m-4 hover:text-[#018053] transition-all duration-500">Already have an account? Login</a>
             </form>
             </div>
